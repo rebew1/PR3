1f56a9c4c1ef2fcc496f5bbaed5a5780d2c1482d
Roman <qweemo4512@gmail.com>
2024-03-24 19:28:19 +0700

    Inital commit

---
+++ PR3-4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n = 0; 
+    int a[20];
+    for (int i = 0; i < 20; i++) 
+    {
+        a[i] = rand();
+        cout << a[i] << " ";
+        if (i % 2 != 0 && i != 0) 
+        { 
+            n += a[i];
+        }
+    }
+    cout << "\n" << "Сумма значений чётных по порядку:" << "\n" << n << "\n";
+}
